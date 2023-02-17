83c69ec7caae548f80111903a3ce13c6ca62cb17
Marina Ustinovich <marishka0509@mail.ru>
2023-02-17 13:34:10 +0300

    added files

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,25 @@
+import { getLevel } from '../app';
+import fetchData from '../http';
+
+jest.mock('../http');
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test('should call getLevel once and status false', () => {
+  fetchData.mockReturnValue({status: 'false', level: 10});
+
+  const response = getLevel(1);
+  expect(response).toBe('Информация об уровне временно недоступна');
+  expect(fetchData).toBeCalledWith('https://server/user/1');
+});
+
+test('should call getLevel once and status ok', () => {
+  fetchData.mockReturnValue({status: 'ok', level: 10});
+
+  const response = getLevel(1);
+  expect(response).toBe('Ваш текущий уровень: 10');
+  expect(fetchData).toBeCalledWith('https://server/user/1');
+});
+
